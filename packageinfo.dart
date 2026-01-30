5b6750d4949f747c55715f905f936725c36a9b86
Seriki yakub <167559384+Web4application@users.noreply.github.com>
2026-01-30 18:20:53 +0100

    Create packageinfo.dart

---
+++ packageinfo.dart
@@ -0,0 +1,15 @@
+class PackageInfo {
+  // ···
+
+  factory PackageInfo.fromJson(Map<String, dynamic> json) {
+    final repository = json['repository'] as String?;
+
+    return PackageInfo(
+      name: json['name'] as String,
+      latestVersion: json['latestVersion'] as String,
+      description: json['description'] as String,
+      publisher: json['publisher'] as String,
+      repository: repository != null ? Uri.tryParse(repository) : null,
+    );
+  }
+}
