f3a24fd849740c6d3688753874b23e1928d5b5ba
Seriki yakub <167559384+Web4application@users.noreply.github.com>
2026-01-30 18:29:16 +0100

    Create decode.dart

---
+++ Package/Http/decode.dart
@@ -0,0 +1,15 @@
+class PackageInfo {
+  final String name;
+  final String latestVersion;
+  final String description;
+  final String publisher;
+  final Uri? repository;
+
+  PackageInfo({
+    required this.name,
+    required this.latestVersion,
+    required this.description,
+    required this.publisher,
+    this.repository,
+  });
+}
